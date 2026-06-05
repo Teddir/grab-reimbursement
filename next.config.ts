dc0829d4d041dc3aebb7d5a1f13644316ff9f827
Teddir <ferryaja20000@gmail.com>
2026-06-05 14:43:23 +0700

    feat: add allowedDevOrigins to next config for development environment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['https://grab-reimbursement.asiasistem.com', '192.168.19.186'],
 };
 
 export default nextConfig;
